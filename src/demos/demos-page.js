daa2f611fa0acf719631895221ed2de65e9c9988
chernokm <mac_chernok@yahoo.com>
2021-03-08 14:37:30 -0600

    (Fake teammate) Updated new h1 "changed" => "Added"

--- src/demos/demos-page.js
+++ src/demos/demos-page.js
@@ -14,7 +14,7 @@ function DemosPage() {
     return (
         <main>
             <h1>My First React App</h1>
-            <h1>Change for github</h1>
+            <h1>Added for github</h1>
 
             <h2>Rendering Arrays</h2>
             <Todos></Todos>
