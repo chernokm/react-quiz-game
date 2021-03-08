ea75162fee2dfbaacb08538f2574a8cadb222155
chernokm <mac_chernok@yahoo.com>
2021-03-08 15:09:35 -0600

    Changed home page for vercel

--- src/home/home-page.js
+++ src/home/home-page.js
@@ -3,7 +3,7 @@ function HomePage() {
     return (
         <main>
             <h1>Home Page</h1>
-            <p>This is the home page</p>
+            <p>Welcom to the home page!</p>
         </main>
     );
 }
